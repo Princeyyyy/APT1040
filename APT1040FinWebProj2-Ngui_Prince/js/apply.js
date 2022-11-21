73bf3192d47bdd11c600ce18bac2b532a9c2472c
Princeyyyy <princemumo80@gmail.com>
2022-11-21 17:06:10 +0300

    finished v1 of application page

---
+++ APT1040FinWebProj2-Ngui_Prince/js/apply.js
@@ -0,0 +1,64 @@
+
+(function ($) {
+    "use strict";
+
+    /*[ Validate ]*/
+    var first_name = $('.validate-input input[name="firstname"]');
+    var last_name = $('.validate-input input[name="lastname"]');
+    var email = $('.validate-input input[name="email"]');
+    var phoneno = $('.validate-input input[name="phoneno"]');
+    var e = document.getElementById("courses");
+    var course = e.value;
+
+
+    $('.validate-form').on('submit',function(){
+        var check = true;
+
+        if($(first_name).val().trim() == ''){
+            showValidate(first_name);
+            check=false;
+        }
+
+        if($(last_name).val().trim() == ''){
+            showValidate(last_name);
+            check=false;
+        }
+
+        if($(phoneno).val().trim() == ''){
+            showValidate(phoneno);
+            check=false;
+        }
+
+
+        if($(email).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+            showValidate(email);
+            check=false;
+        }
+
+        if($(course).val().trim() == 'default'){
+            showValidate(course);
+            check=false;
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input1').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+       });
+    });
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+})(jQuery);
