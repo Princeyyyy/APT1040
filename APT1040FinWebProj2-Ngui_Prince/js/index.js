0db8706570419090c5ad6d70f9ab57ef5bbfc540
Princeyyyy <princemumo80@gmail.com>
2022-11-22 22:43:49 +0300

    minor updates

--- APT1040FinWebProj2-Ngui_Prince/js/index.js
+++ APT1040FinWebProj2-Ngui_Prince/js/index.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Excellence", "Innovation", "Passion"],
+        strings: ["Excellence", "Dedication", "Passion"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
